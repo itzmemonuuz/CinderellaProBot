885dc44c8bb8390ebc03957cf30b139cd9867877
Ashraf monuuz <85083304+itzmemonuuz@users.noreply.github.com>
2021-05-31 10:12:32 +0530

    Update __main__.py

--- cinderella/__main__.py
+++ cinderella/__main__.py
@@ -58,7 +58,8 @@ def vercheck() -> str:
 
 
 SOURCE_STRING = """
-⚡I'm built in python3, using the python-telegram-bot library, and am fully opensource - you can find what makes me tick [here](https://github.com/Sur-vivor/CinderellaProBot)
+⚡I'm built in python3, using the python-telegram-bot library, and am fully opensource - you can find what makes me tick [here](https://github.com/S
+itzmemonuuz/CinderellaProBot)
 ⚡You Can Clone Me [Here](https://heroku.com/deploy?template=https://github.com/itzmemonuuz/CinderellaProBot.git)
 """
 
